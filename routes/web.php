1c7694be14d89f5c55aea5d26f1a9b1e732e9bea
Stef <67269037+Stef-C@users.noreply.github.com>
2024-06-18 00:49:40 +0800

    Added routes for about me, skills and hobbies pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
+
+Route::get('/about-me', [AboutMeController::class, 'index'])->name('about.me');
+Route::get('/skills', [SkillsController::class, 'index'])->name('skills');
+Route::get('/hobbies', [HobbiesController::class, 'index'])->name('hobbies');
