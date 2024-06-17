a6e142befd5b5c08c2393900fe9e3389079f361a
Stef <67269037+Stef-C@users.noreply.github.com>
2024-06-18 00:58:28 +0800

    Created views for about me, skills and hobbies pages

---
+++ resources/views/skills.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Skills</title>
+</head>
+<body>
+    <h1>{{ $data }}</h1>
+    <button onclick="location.href='{{ route('about.me') }}'">About Me</button>
+    <button onclick="location.href='{{ route('skills') }}'">Skills</button>
+    <button onclick="location.href='{{ route('hobbies') }}'">Hobbies</button>
+</body>
+</html>
