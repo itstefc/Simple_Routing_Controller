fb9c6279f0158626d93a59e94a63d615e483883a
Stef <67269037+Stef-C@users.noreply.github.com>
2024-06-18 01:14:11 +0800

    Set default route to About me page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/', [AboutMeController::class, 'index'])->name('about.me');
 
-
-use App\Http\Controllers\AboutMeController;
-use App\Http\Controllers\SkillsController;
-use App\Http\Controllers\HobbiesController;
 
 Route::get('/about-me', [AboutMeController::class, 'index'])->name('about.me');
 Route::get('/skills', [SkillsController::class, 'index'])->name('skills');
